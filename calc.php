2ca2818c45bffb821fdfb774a4a18f05e9aa8cbb
Priyanshu Chouhan <priyanshu.chouhan11.pc@gmail.com>
2020-09-19 12:47:43 +0530

    DSC Task

---
+++ calc.php
@@ -0,0 +1,62 @@
+<?php 
+	
+	$errors = "";
+
+	// connect to database
+	$db = mysqli_connect("localhost", "root", "", "mydb");
+	// select all tasks if page is visited or refreshed
+	$tasks = mysqli_query($db, "SELECT * FROM users");
+	if(isset($_GET['del'])){
+		$s="TRUNCATE TABLE users";
+		mysqli_query($db,$s);
+	}
+
+?>
+
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Calculated</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<table>
+		<thead>
+			<tr>
+				<th>No.</th>
+				<th>Participants</th>
+				<th>Amount</th>
+				<th style="width: 60px;">Owes</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php $i = 1;
+			$nu=mysqli_num_rows($tasks);
+			$myfile = fopen("newfile.txt", "r");
+			$total=fread($myfile,filesize("newfile.txt"));
+			if($nu>0){
+			$avg=$total/$nu;}
+			while ($rows = mysqli_fetch_array($tasks)) { 
+						$a=$rows['amt'];
+						$owe=$a-$avg;
+						$id=$rows['id'];
+						$sq="UPDATE users SET owes=$owe WHERE id=$id";
+						mysqli_query($db,$sq);
+						//header('location:calc.php');
+			?>
+				<tr>
+					<td> <?php echo $i; ?> </td>
+					<td class="split"> <?php echo $rows['name']; ?> </td>
+					<td class="split"> <?php echo $rows['amt'];?></td>
+					<td class="split"> <?php echo $rows['owes']?></td>
+				</tr>
+			<?php $i++; }?>
+		</tbody>
+	</table>
+	<a href="calc.php?del" class="add_btn" id="add_btn">Clear All Records</a>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,102 @@
+<?php 
+	
+	$errors = "";
+
+	// connect to database
+	$db = mysqli_connect("localhost", "root", "", "mydb");
+
+	// insert a quote if submit button is clicked
+	if (isset($_POST['add'])) {
+
+		if (empty($_POST['part']) or empty($_POST['amt'])) {
+			$errors = "You must fill in the vlaues";
+		}else{
+			$name = $_POST['part'];
+			$amt=$_POST['amt'];
+			$query = "INSERT INTO users (name,amt) VALUES ('$name',$amt)";
+			mysqli_query($db, $query);
+			header('location: index.php');
+		}
+	}	
+
+	// delete task
+	if (isset($_GET['del_task'])) {
+		$id = $_GET['del_task'];
+
+		mysqli_query($db, "DELETE FROM users WHERE id=".$id);
+		header('location: index.php');
+	}
+	
+	
+
+	// select all tasks if page is visited or refreshed
+	$tasks = mysqli_query($db, "SELECT * FROM users");
+
+?>
+<!DOCTYPE html>
+<html>
+
+<head>
+	<title>Split your BILL</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+
+	<div class="heading">
+		<h2 style="font-style: 'Hervetica';">Split your BILL</h2>
+	</div>
+
+
+	<form method="post" action="index.php" class="input_form">
+		<?php if (isset($errors)) { ?>
+			<p><?php echo $errors; ?></p>
+		<?php } ?>
+		<input type="text" name="part" class="split_input" placeholder="Participant name">
+		<input type="number" name="amt" class="split_input" placeholder="Amount">
+		<button type="submit" name="add" id="add_btn" class="add_btn">Add</button>
+	
+
+
+	<table>
+		<thead>
+			<tr>
+				<th>No.</th>
+				<th>Participants</th>
+				<th>Amount</th>
+				<th style="width: 60px;">Delete</th>
+			</tr>
+		</thead>
+
+		<tbody>
+			<?php $i = 1;
+			$total=0;
+			while ($row = mysqli_fetch_array($tasks)) { ?>
+				<tr>
+					<td> <?php echo $i; ?> </td>
+					<td class="split"> <?php echo $row['name']; ?> </td>
+					<td class="split"> <?php echo $row['amt'];?></td>
+					<td class="delete"> 
+						<a href="index.php?del_task=<?php echo $row['id']; ?>">x</a> 
+					</td>
+				</tr>
+			<?php	$total+=$row['amt'];
+					$i++; } 
+			$myfile = fopen("newfile.txt", "w");
+			fwrite($myfile,$total);
+			fclose($myfile);?>
+			<tr>
+				<td></td>
+				<td class="split">TOTAL</td>
+				<td class="split"><?php echo $total; ?></td>
+				<td></td>
+			</tr>
+			
+		</tbody>
+	</table>
+	<a href="calc.php" class="add_btn" id="add_btn">Calculate</a>
+	</form>
+	
+
+</body>
+</html>
